1d99cdbabef496f05c6fae9ba9277947b85df2c5
Napo <Michaeljnapolitano@gmail.com>
2020-10-10 13:05:46 -0400

    general employee questions

--- Index.js
+++ Index.js
@@ -1,3 +1,49 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+const manager = require('./lib/Manager');
+const engineer = require('./lib/Engineer');
+const intern = require('./lib/Intern');
+
+let team = []
+
+const employeeQuestions = [{
+    type: 'input',
+    name: 'name',
+    message: 'Enter employee name (required)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a name!');
+            return false;
+        }
+    },
+    type: 'input',
+    name: 'id',
+    message: 'Enter an employee ID (required)',
+    validate: idInput => {
+        if (idInput) {
+            return true;
+        } else {
+            console.log('Please enter an ID!');
+            return false;
+        }
+    },
+    type: 'input',
+    name: 'email',
+    message: 'Enter an employee email (required)',
+    validate: emailInput => {
+        if (emailInput) {
+            return true;
+        } else {
+            console.log('Please enter an email!');
+            return false;
+        }
+    },
+    type: 'list',
+    name: 'type',
+    message: 'Enter employee type',
+    choices: ["Manager", "Engineer", "Intern"]
+    },
+]
