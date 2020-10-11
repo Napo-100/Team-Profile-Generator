d6056a4b7bf18ec472aac65aecd895dab827e3f7
Napo <Michaeljnapolitano@gmail.com>
2020-10-11 19:48:04 -0400

    employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,29 @@
+const { TestScheduler } = require('jest');
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Mike');
+    
+    expect(employee.name).toBe('Mike');
+    expect(typeof(employee)).toBe('object');
+    
+});
+
+test('gets employee name', () => {
+    const employee = new Employee();
+
+    expect(employee.getName()).toBe(this.name);
+});
+
+test('gets employee id', () => {
+    const employee = new Employee();
+
+    expect(employee.getID()).toBe(this.id);
+});
+
+test('gets employee email', () => {
+    const employee = new Employee();
+
+    expect(employee.getEmail()).toBe(this.email);
+});
+
